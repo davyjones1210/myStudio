299868eb42cf746946cf2456a2f44f952a3eb8ad
Davy Jones <kunal.dekhane@gmail.com>
2025-02-08 18:01:13 -0500

    Revert "Commiting day 2 exercises and files"
    This reverts commit 4fcd0ce0dbfef3849d0666ea0bcf30ceefec815b.

--- bin/cat-maya.py
+++
@@ -1,6 +0,0 @@
-import os
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logging.info("Launching my cat command to start Maya")
-os.system('"E:\\AutoDesk\\Maya2025\\bin\\maya.exe"')
--- bin/test_01.py
+++ bin/test_01.py
@@ -1,1 +1,1 @@
-print("Hello world in Python file")
+print("Hello world")
