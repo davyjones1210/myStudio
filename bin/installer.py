4b018ef1fc89838ec6438197a2b67a2d631d111b
Kunal Dekhane <kunal.dekhane@gmail.com>
2025-02-10 15:03:26 -0500

    Update bin/installer.py
    Ok, commiting changes by you and syncing to my local repo
    
    Co-authored-by: Subin Gopi <36088306+subing85@users.noreply.github.com>

--- bin/installer.py
+++ bin/installer.py
@@ -1,24 +1,51 @@
 # This is to setup the studio environment
 
-# import optparse
-# print(help(optparse))
+#!/bin/bash
 
-# import OptionParser class 
-# from optparse module.
-from optparse import OptionParser
+import logging
+import optparse
 
-# create a OptionParser
-# class object
-parser = OptionParser()
+logging.basicConfig(level=logging.INFO)
 
-# add options
-parser.add_option("-e", "--environment",
-				dest = "environment",
-				help = "to set environment to our studio", 
-				metavar = "FILE")
+def execute():
 
-(options, args) = parser.parse_args()
+    # python3 '/home/batman/Desktop/test/code_01.py'
+    # python3 '/home/batman/Desktop/test/code_01.py' -e
+    # python3 '/home/batman/Desktop/test/code_01.py' --open "belnder4.3.1"
 
-print(options, args)
+    parser = optparse.OptionParser(
+       usage="usage: %prog\n\tIntaller to set up my studio pipeline",
+       version="0.0.1",
+    )
 
+    parser.add_option(
+        "-e", "--environment",
+        action = "store_true", 
+        dest = "environment",
+        default = False,
+        help = "Set up the pipeline command and primary environments"
+    )
+    
+    parser.add_option(     
+        "-o", "--open",
+        dest="open",
+        action="store",
+        help="To lauch(Open) DCC (Digital Command Control).",     
+    )
+     
+    options, args = parser.parse_args()
+ 
+    print(options, args)
+    print("\n")
+    
+    if options.environment:
+        print("\tenvironment", options.environment)
+        logging.info("\tStart to setp my studio envs")
+    
+    if options.open:
+        print("\tCurrent software", options.open)
+
+
+if __name__ == "__main__":    
+   execute()
 
